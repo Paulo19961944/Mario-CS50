43bf2a7208bfdfe9187d3fc4cf06cf6c15903d9e
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2024-11-02 14:27:04 -0300

    feature: Add New Function

--- hashes.h
+++ hashes.h
@@ -7,4 +7,7 @@ int get_int(const char *mensagem);
 // Função para criar e imprimir hashes
 void hashesFactory(int num);
 
+// Função para criar e imprimir triangulo
+void triangleFactory(int num);
+
 #endif // HASHES_H
