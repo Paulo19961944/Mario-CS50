a5cda279e49e3afba66eaa1c6e8e54cfb28097c2
Paulo Henrique <paulo24041996@gmail.com>
2024-11-02 11:58:47 -0300

    feature: Add Triangle in Library

--- hashes.c
+++ hashes.c
@@ -37,3 +37,16 @@ void hashesFactory(int num) {
         printf("\n");
     }
 }
+
+// FUNÇÃO PARA CRIAR UM TRIANGULO
+void triangleFactory(int num){
+    for(int i = 0; i < num; i++){
+        for(int j = 0; j < num - i - 1; j++){
+            printf(" ");
+        }
+        for(int j = 0; j <= i; j++){
+            printf(" #");
+        }
+        printf("\n");
+    }
+}
