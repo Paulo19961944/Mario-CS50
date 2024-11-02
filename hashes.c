6b38af3eacb9a842d3ab596b111ef777fa11b63a
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2024-11-02 20:04:32 -0300

    feature: Add New Function

--- hashes.c
+++ hashes.c
@@ -25,6 +25,11 @@ int get_int(const char *mensagem) {
     }
 }
 
+// FUNÇÃO PARA PULAR LINHA
+void skipNextLine() {
+    printf("\n");
+}
+
 // FUNÇÃO PARA FABRICAR OS HASHES
 void hashesFactory(int num) {
     for (int i = 0; i < num; i++) {
@@ -34,19 +39,44 @@ void hashesFactory(int num) {
         for (int j = 0; j <= i; j++) {
             printf("#");
         }
-        printf("\n");
+        skipNextLine();
     }
 }
 
 // FUNÇÃO PARA CRIAR UM TRIANGULO
-void triangleFactory(int num){
-    for(int i = 0; i < num; i++){
-        for(int j = 0; j < num - i - 1; j++){
+void triangleFactory(int num) {
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num - i - 1; j++) {
             printf(" ");
         }
-        for(int j = 0; j <= i; j++){
-            printf(" #");
+        for (int j = 0; j <= i; j++) {
+            printf(" #"); 
         }
-        printf("\n");
+        skipNextLine();
+    }
+}
+
+// FUNÇÃO PARA CRIAR TRIANGULO COM ESPAÇOS
+void triangleSpacesFactory(int num) {
+    for (int i = 0; i < num; i++) {
+        // Impressão dos espaços à esquerda
+        for (int j = 0; j < num - i - 1; j++) {
+            printf(" ");
+        }
+
+        // Impressão do primeiro grupo de #
+        for (int j = 0; j <= i; j++) {
+            printf("#");
+        }
+
+        // Espaço entre os dois grupos de #
+        printf(" "); // Adiciona um espaço
+
+        // Impressão do segundo grupo de #
+        for (int j = 0; j <= i; j++) {
+            printf("#");
+        }
+
+        skipNextLine(); 
     }
 }
